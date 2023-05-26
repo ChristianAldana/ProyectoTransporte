dd4d34080fc066b28e57ad4876af202550fb1015
Christian <caldanas2@miumg.edu.gt>
2023-05-26 14:43:30 -0600

    Se agrego plantilla modificada de email

---
+++ resources/views/auth/passwords/email.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/estilos.css') }}">
+
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10">
+                    <div class="card rounded-3 text-black">
+                        <div class="row g-0">
+                            <div class="col-lg-6">
+                                <div class="card-body p-md-5 mx-md-4">
+                                    <div class="text-center">
+                                        <img src="{{ asset('assets/logo.jfif') }}"
+                                             style="width: 250px;" alt="logo">
+                                        <h4 class="mt-1 mb-5 pb-1">Transportes Internacional S.A</h4>
+                                    </div>
+
+                                    @if (Session::has('status'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ Session::get('status') }}
+                                        </div>
+                                    @endif
+
+                                    <form method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+
+                                        <div class="row mb-3">
+                                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+
+                                        <div class="d-flex align-items-center justify-content-center pb-4">
+                                            <p class="mb-0 me-2"></p>
+                                            <button type="submit" class="btn btn-outline-danger"> {{ __('Send Password Reset Link') }}
+                                            </button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h4 class="mb-4">We are more than just a company</h4>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
