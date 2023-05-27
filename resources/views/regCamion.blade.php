d89ca1febbd84a0a3aa82e3fea01fe7b9529e7f3
Suzzanne20 <susanacevedo11@hotmail.com>
2023-05-26 20:46:04 -0600

    Creacion vista registro camion

---
+++ resources/views/regCamion.blade.php
@@ -0,0 +1,71 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<link href="{{ asset('assets/form.css') }}" rel="stylesheet">
+
+<div class="container contact">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="contact-info">
+				<img src="https://raw.githubusercontent.com/Suzzanne20/ResourceNekoStation/main/kisspng-computer-icons-truck-font-awesome-couriers-vector-5ae0b656310a78.1827635715246761822009.png" width="90"/>
+				<h2>Camión</h2>
+				<h4>Registro de nuevos Camiones</h4>
+			</div>
+		</div>
+		<div class="col-md-9">
+			<div class="contact-form">
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="Matricula">Matricula</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="Matricula" placeholder="Ingresa la matricula" name="Matricula">
+				  </div>
+				</div>
+				<div class="form-group">
+				  <label class="control-label col-sm-4" for="marca">Marca</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="marca" placeholder="Ingresa la Marca" name="marca">
+				  </div>
+				</div>
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="modelo">Modelo</label>
+				  <div class="col-sm-10">
+					<input type="text" class="form-control" id="modelo" placeholder="Ingresa el modelo" name="modelo">
+				  </div>
+				</div>
+
+                <div class="form-group">
+				  <label class="control-label col-sm-2" for="cap">Capacidad</label>
+				  <div class="col-sm-10">
+					<input type="number" class="form-control" id="cap" placeholder="Ingresa la capacidad en Lb" name="cap">
+				  </div>
+				</div>
+
+                <div class="form-group">
+				  <label class="control-label col-sm-2" for="cap">Transportista</label>
+				  <div class="col-12">
+                  <select class="form-select" aria-label="Default select example">
+                    <option selected>Selecciona la Transportista</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                    </select>
+				  </div>
+				</div>
+
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="comment">Comment:</label>
+				  <div class="col-sm-10">
+					<textarea class="form-control" rows="5" id="comment"></textarea>
+				  </div>
+				</div>
+				<div class="form-group">        
+				  <div class="col-sm-offset-2 col-sm-10">
+                  <button type="submit" class="btn btn-default">Submit</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- resources/views/regTransport.blade.php
+++ resources/views/regTransport.blade.php
@@ -1,14 +1,15 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+
+
 <link href="{{ asset('assets/form.css') }}" rel="stylesheet">
 
 <div class="container contact">
 	<div class="row">
 		<div class="col-md-3">
 			<div class="contact-info">
-				<img src="https://img2.freepng.es/20180320/sfq/kisspng-pickup-truck-van-car-mover-vector-truck-drawing-5ab0b07ab3d2e2.7109356215215289547366.jpg" alt="image"/>
+				<img src="https://raw.githubusercontent.com/Suzzanne20/ResourceNekoStation/main/kisspng-computer-icons-truck-font-awesome-couriers-vector-5ae0b656310a78.1827635715246761822009.png" width="90"/>
 				<h2>Transportista</h2>
 				<h4>Registro de nuevos Transportistas</h4>
 			</div>
@@ -16,21 +17,21 @@
 		<div class="col-md-9">
 			<div class="contact-form">
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="fname">First Name:</label>
+				  <label class="control-label col-sm-2" for="fname">Nombre</label>
 				  <div class="col-sm-10">          
-					<input type="text" class="form-control" id="fname" placeholder="Enter First Name" name="fname">
+					<input type="text" class="form-control" id="fname" placeholder="Ingresa el Nombre" name="fname">
 				  </div>
 				</div>
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="lname">Last Name:</label>
+				  <label class="control-label col-sm-4" for="lname">Razon Social</label>
 				  <div class="col-sm-10">          
-					<input type="text" class="form-control" id="lname" placeholder="Enter Last Name" name="lname">
+					<input type="text" class="form-control" id="lname" placeholder="Ingresa la Razon Social" name="lname">
 				  </div>
 				</div>
 				<div class="form-group">
-				  <label class="control-label col-sm-2" for="email">Email:</label>
+				  <label class="control-label col-sm-2" for="direccion">Direccion</label>
 				  <div class="col-sm-10">
-					<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+					<input type="text" class="form-control" id="direccion" placeholder="Ingresa la direccion" name="direccion">
 				  </div>
 				</div>
 				<div class="form-group">
@@ -41,7 +42,7 @@
 				</div>
 				<div class="form-group">        
 				  <div class="col-sm-offset-2 col-sm-10">
-					<button type="submit" class="btn btn-default">Submit</button>
+                  <button type="submit" class="btn btn-default">Submit</button>
 				  </div>
 				</div>
 			</div>
